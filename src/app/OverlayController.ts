99c1245e6afc840364845db0d02f9d46734d2054
david <codiladavid@yahoo.com.au>
2026-01-24 17:31:43 +1100

    fix start page

--- src/app/OverlayController.ts
+++ src/app/OverlayController.ts
@@ -3,21 +3,21 @@ import { TransitionController } from "./transition/TransitionController";
 export class OverlayController {
 
     public static startFadeSequence(): void {
-        const overlayElement = document.getElementById('ui-overlay');
+        const overlayElement = document.getElementById('start-page');
         if (!overlayElement) return;
 
         overlayElement.style.transition = 'opacity 2s ease-out';
         overlayElement.style.opacity = '0';
         overlayElement.style.pointerEvents = 'none';
         TransitionController.getInstance().activate();
-        OverlayController.removeAndLock(1500, () => {
+        OverlayController.removeAndLock(2000, () => {
             const canvas = document.querySelector('canvas');
             canvas?.requestPointerLock();
         });
     }
 
     private static removeAndLock(delayMilliseconds: number, onComplete: () => void): void {
-        const overlayElement = document.getElementById('ui-overlay');
+        const overlayElement = document.getElementById('start-page');
         setTimeout(() => {
             overlayElement?.remove();
             onComplete();
